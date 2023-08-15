8ca6ccab50f8a11071cc44a02186b43f7279c3a3
oyun <oyun@unifonic.com>
2023-08-15 10:40:11 +0600

    add seed

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,45 +1,70 @@
 with
-orders as 
-(
+
+orders as (
+
     select * from {{ ref('stg_tech_store__orders') }}
+
 ),
-transactions as 
-(
+
+transactions as (
+
     select * from {{ ref('stg_payment_app__transactions') }}
+
 ),
-products as 
-(
+
+products as (
+
     select * from {{ ref('stg_tech_store__products') }}
+
 ),
-customers as 
-(
+
+customers as (
+
     select * from {{ ref('stg_tech_store__customers') }}
+
 ),
-final as 
-(
-select
-                orders.order_id,
-                transactions.transaction_id,
-                customers.customer_id,
-                customers.customer_name,
-                products.product_name,
-                products.category,
-                products.price,
-                products.currency,
-                orders.quantity,        
-                transactions.cost_per_unit_in_usd,
-                transactions.amount_in_usd,
-                transactions.tax_in_usd,
-                transactions.total_charged_in_usd,
-                orders.created_at
-from            orders
-left join       transactions
-on              orders.order_id = transactions.order_id
 
-left join       products
-on              orders.product_id = products.product_id
+sale_dates as (
 
-left join       customers
-on              orders.customer_id = customers.customer_id
+    select * from {{ ref('sale_dates') }}
+
+),
+
+
+final as (
+
+    select
+        orders.order_id,
+        transactions.transaction_id,
+        customers.customer_id,
+        customers.customer_name,
+        products.product_name,
+        products.category,
+        products.price,
+        products.currency,
+        orders.quantity,
+        sale_dates.sale_date is not null as is_sale_order,
+        nvl(sale_dates.discount_percent, 0) as discount_percent, 
+        transactions.cost_per_unit_in_usd,
+        transactions.amount_in_usd,
+        transactions.tax_in_usd,
+        transactions.total_charged_in_usd,
+        orders.created_at
+
+    from orders
+
+    left join transactions
+        on orders.order_id = transactions.order_id
+
+    left join products
+        on orders.product_id = products.product_id
+
+    left join customers
+        on orders.customer_id = customers.customer_id
+
+    left join sale_dates
+        on date(orders.created_at) = date(sale_dates.sale_date)
+
 )
+
 select * from final
